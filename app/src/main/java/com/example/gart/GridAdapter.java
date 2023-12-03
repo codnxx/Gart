a1e0b457f8f1769346cee5c5f4fb36583f23e2a1
noweaqc <chaevly0818@naver.com>
2023-12-03 17:04:46 +0900

    all

--- app/src/main/java/com/example/gart/GridAdapter.java
+++ app/src/main/java/com/example/gart/GridAdapter.java
@@ -11,6 +11,8 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.request.RequestOptions;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -35,11 +37,19 @@ public class GridAdapter extends RecyclerView.Adapter<GridAdapter.ViewHolder> {
             String cultureName = data.get(position); // 데이터 가져오기
             holder.title.setText(cultureName); // 데이터를 항목에 설정
 
+            RequestOptions requestOptions = new RequestOptions();
+            requestOptions = requestOptions.dontAnimate(); // 디코딩 오류 회피
+
             // 이미지 로딩 라이브러리를 사용하여 이미지 설정
-            String imageUrl = "http://13.124.226.102:8080/gart/" + (position-1);
-            System.out.println(position-1);
+            String imageUrl = "http://13.124.226.102:8080/gart/image?title=" + cultureName;
+            System.out.println(imageUrl);
             Glide.with(holder.itemView.getContext())
                     .load(imageUrl)
+                    .apply(requestOptions)
+                    .centerCrop()
+                    .diskCacheStrategy(DiskCacheStrategy.NONE)
+                    .placeholder(R.drawable.ic_launcher_foreground)
+                    .error(R.drawable.ic_launcher_foreground)
                     .into(holder.img);
         }
     }
@@ -51,7 +61,7 @@ public class GridAdapter extends RecyclerView.Adapter<GridAdapter.ViewHolder> {
 
     @Override
     public int getItemCount() {
-        return 7; // 항목 수를 변경하려면 원하는 숫자로 수정
+        return 4; // 항목 수를 변경하려면 원하는 숫자로 수정
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
